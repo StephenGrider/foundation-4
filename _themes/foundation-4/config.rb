b40c058d23fcc11e866d88ee48e82cc6554c11a1
Gareth Redfern <gareth.redfern@gmail.com>
2013-03-09 07:50:07 +0000

    Added file path to theme & fonts.

--- _themes/foundation-4/config.rb
+++ _themes/foundation-4/config.rb
@@ -3,11 +3,12 @@ require 'zurb-foundation'
 
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/_themes/foundation-4/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
